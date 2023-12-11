58a6883c991cc0f7ec876a7cd179792d87342efd
Kyxxn <gywns626@naver.com>
2023-12-12 04:05:44 +0900

    [ADD] Animator Finish

--- Assets/Scripts/PHJ/HJ_GameManager.cs
+++ Assets/Scripts/PHJ/HJ_GameManager.cs
@@ -7,15 +7,6 @@ public class HJ_GameManager : MonoBehaviour
     [SerializeField]
     private HJ_GameMode HJMode = HJ_GameMode.instance;
 
-    private void OnParticleCollision(GameObject other)
-    {
-        if (HJMode.PHealth <= 0)
-        {
-            gameObject.SetActive(false);
-            //플레이어 담구기
-            HJMode.GameOver();
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Exit")
