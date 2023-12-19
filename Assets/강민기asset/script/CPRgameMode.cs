93829252d08e543f66651fdcc7ba4e89f6b6fc17
Kyxxn <gywns626@naver.com>
2023-12-19 22:36:23 +0900

    [FIX] 한글 깨짐 개선

--- Assets/강민기asset/script/CPRgameMode.cs
+++ Assets/강민기asset/script/CPRgameMode.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditor.Timeline.Actions;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
